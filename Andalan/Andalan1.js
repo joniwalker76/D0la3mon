ac0e78724a01751b4838f7981a34a25c10d13383
joniwalker76 <amaliawanti3@gmail.com>
2025-09-30 22:45:31 +0700

    Update Andalan1.js

--- Andalan/Andalan1.js
+++ Andalan/Andalan1.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      3.1
 // @description  try to take over the world!
-// @updateURL    https://raw.githubusercontent.com/joniwalker76/Doraemon/refs/heads/main/Andalan/Andalan1.js?token=GHSAT0AAAAAADMHAFEG7GIXTA7KYP4ZJPZI2G36EVA
+// @updateURL    https://raw.githubusercontent.com/joniwalker76/Doraemon/refs/heads/main/Andalan/Andalan1.js
 // @downloadURL  https://raw.githubusercontent.com/joniwalker76/Doraemon/refs/heads/main/Andalan/Andalan1.js?token=GHSAT0AAAAAADMHAFEG7GIXTA7KYP4ZJPZI2G36EVA
 // @author       You
 // @match        http*://*/*
