0e088aa77a68fd26d617363c76518aae20b455a7
joniwalker76 <amaliawanti3@gmail.com>
2025-11-18 00:06:06 +0700

    Update Makmur1.js

--- Makmur/Makmur1.js
+++ Makmur/Makmur1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Makmur1
 // @namespace    http://tampermonkey.net/
-// @version      3.1
+// @version      3.2
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/joniwalker76/D0la3mon/refs/heads/main/Makmur/Makmur1.js
 // @downloadURL  https://raw.githubusercontent.com/joniwalker76/D0la3mon/refs/heads/main/Makmur/Makmur1.js
@@ -33,7 +33,7 @@ var Comment6 = 'GOHTOGEL=FRIZA66=78*69*72';
 var namagroup7 = 'VESPA7093L';
 var Comment7 = 'VESPATOGEL (FLEX77) 83*30*08';
 var namagroup8 = 'ANGKER';
-var Comment8 = 'ANGKER4D=STATIC88=83*30*08';
+var Comment8 = 'ANGKER4D=ZERO00=83*30*08';
 var namagroup9 = 'BUKU';
 var Comment9 = 'IYATOTO HERCULES99 48*19*57';
 var namagroup10 = 'JNE';
@@ -63,7 +63,7 @@ var Comment18 = 'Makmur 1';
 var SCRIPT_NAME = Comment18
 var refresh = 40;
 var URLADMIN = "https://raw.githubusercontent.com/joniwalker76/D0la3mon/refs/heads/main/Admin.json"
-var keyword = ["ROOM","𝗥𝗢𝗢𝗠","LOMBA","𝗟𝗢𝗠𝗕𝗔","𝐋𝐎𝐌𝐁𝐀","LIMBA","ROM","R00M","login","𝐑𝐎𝐎𝐌","HONGKONG","SINGAPUR","nemo","l0mb4","lomb4","l0mba","𝗥𝟬𝟬𝗠","𝗟𝟬𝗠𝗕𝗔","𝘙𝘖𝘖𝘔","hatori","klikh4tori001"]
+var keyword = ["▇ █","ROOM","𝗥𝗢𝗢𝗠","LOMBA","𝗟𝗢𝗠𝗕𝗔","𝐋𝐎𝐌𝐁𝐀","LIMBA","ROM","R00M","login","𝐑𝐎𝐎𝐌","HONGKONG","SINGAPUR","nemo","l0mb4","lomb4","l0mba","𝗥𝟬𝟬𝗠","𝗟𝟬𝗠𝗕𝗔","𝘙𝘖𝘖𝘔","hatori","klikh4tori001"]
 var Backlist =["pemenang lomba","rekap","natidulu","room lomba freebet","prediksi","result","juara lomba","r3k4p","r3kap","rek4p","undang" ]
 var isCommenting = false;
 var EXPIRATION_MS = 8 * 60 * 1000; // 5 minutes
