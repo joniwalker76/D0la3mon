342b677e186a7f75edfcc730e5520c4c89e7e86c
joniwalker76 <amaliawanti3@gmail.com>
2025-12-27 21:18:02 +0700

    Update Makmur3.js

--- Makmur/Makmur3.js
+++ Makmur/Makmur3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Makmur3
 // @namespace    http://tampermonkey.net/
-// @version      3.5
+// @version      3.6
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/joniwalker76/D0la3mon/refs/heads/main/Makmur/Makmur3.js
 // @downloadURL  https://raw.githubusercontent.com/joniwalker76/D0la3mon/refs/heads/main/Makmur/Makmur3.js
@@ -48,10 +48,10 @@ var namagroup14 = 'KEI';
 var Comment14 = '#keitogel = (55SINGGALANG) = 53*98*50';
 var namagroup15 = 'TOYIB';
 var Comment15 = '#TOYIBSLOT ( 88TANDIKAT ) : 38*93*59';
-var namagroup16 = 'MAYAPADA';
-var Comment16 = 'BETT*Mayapada4D(BO)*77KERINCI*38*93*23';
+var namagroup16 = 'HAT0R1';
+var Comment16 = '#HATORIBET*77KERINCI*38*93*23';
 var namagroup17 = 'timur';
-var Comment17 = 'utara';
+var Comment17 = 'barat';
 
 
 //Batas
