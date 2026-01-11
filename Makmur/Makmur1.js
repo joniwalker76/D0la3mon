e8d678760262006d7cc62cfab6ac931f204c7a1d
joniwalker76 <amaliawanti3@gmail.com>
2026-01-11 16:28:55 +0700

    Update Makmur1.js

--- Makmur/Makmur1.js
+++ Makmur/Makmur1.js
@@ -61,7 +61,7 @@ var Comment18 = 'Makmur 1';
 
 
 var SCRIPT_NAME = Comment18
-var refresh = 40;
+var refresh = 60;
 var URLADMIN = "https://raw.githubusercontent.com/joniwalker76/D0la3mon/refs/heads/main/Admin.json"
 var keyword = ["▇ █","ROOM","𝗥𝗢𝗢𝗠","LOMBA","𝗟𝗢𝗠𝗕𝗔","𝐋𝐎𝐌𝐁𝐀","LIMBA","ROM","R00M","login","𝐑𝐎𝐎𝐌","HONGKONG","SINGAPUR","nemo","l0mb4","lomb4","l0mba","𝗥𝟬𝟬𝗠","𝗟𝟬𝗠𝗕𝗔","𝘙𝘖𝘖𝘔","hatori","klikh4tori001"]
 var Backlist =["pemenang","rekap","natidulu","r3k4p","r3kap","rek4p" ]
