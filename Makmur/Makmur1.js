98638b5865f4478ae8220ecb439889dcc24ededf
joniwalker76 <amaliawanti3@gmail.com>
2026-02-06 03:32:46 +0700

    Update Makmur1.js

--- Makmur/Makmur1.js
+++ Makmur/Makmur1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Makmur1
 // @namespace    http://tampermonkey.net/
-// @version      4.5
+// @version      4.6
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/joniwalker76/D0la3mon/refs/heads/main/Makmur/Makmur1.js
 // @downloadURL  https://raw.githubusercontent.com/joniwalker76/D0la3mon/refs/heads/main/Makmur/Makmur1.js
