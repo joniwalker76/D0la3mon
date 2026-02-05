a48eeaa4c661b4083d66402dcdcefd296cb263df
joniwalker76 <amaliawanti3@gmail.com>
2026-02-06 03:30:53 +0700

    Update Hasil3.js

--- Hasil/Hasil3.js
+++ Hasil/Hasil3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Hasil3
 // @namespace    http://tampermonkey.net/
-// @version      4.2
+// @version      4.3
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/joniwalker76/D0la3mon/refs/heads/main/Hasil/Hasil3.js
 // @downloadURL  https://raw.githubusercontent.com/joniwalker76/D0la3mon/refs/heads/main/Hasil/Hasil3.js
@@ -219,7 +219,7 @@ if(document.location.href.includes("group")){
                     if (tombol.length >= 2) {
                         cekTombolUrutkan = false;
                         tombol.forEach(btn => {
-                            if (countA < 0) {
+                            if (countA < 3) {
                                 if (btn.textContent.includes("Postingan baru")) {
                                     btn.click();
                                     countA++;
@@ -228,7 +228,7 @@ if(document.location.href.includes("group")){
                                 setTimeout(() => {
                                     if (btn.textContent.includes("Aktivitas terbaru")) {
                                         btn.click();
-                                        countA = 3;
+                                        countA = 0;
                                     }
                                 }, 100);
                             }
